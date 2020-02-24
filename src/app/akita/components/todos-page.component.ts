946f36f614dcb78f54a52bfefbbc66fcfcf68b76
svensori <svensori@gmail.com>
2020-02-24 23:32:02 +0800

    Configure project with SASS

--- src/app/akita/components/todos-page.component.ts
+++ src/app/akita/components/todos-page.component.ts
@@ -9,7 +9,7 @@ import { TodosService } from '../services/todos.service';
 @Component({
   selector: 'app-todos-page',
   templateUrl: './todos-page.component.html',
-  styleUrls: ['./todos-page.component.css']
+  styleUrls: ['./todos-page.component.scss']
 })
 export class TodosPageComponent implements OnInit {
 
