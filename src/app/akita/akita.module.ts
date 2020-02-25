dc7d93854b86117ae87d1a747d735aae3c76a8ca
svensori <svensori@gmail.com>
2020-02-25 11:43:30 +0800

    Decouple Modules

--- src/app/akita/akita.module.ts
+++ src/app/akita/akita.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
 
 import { TodosPageComponent } from './components/todos-page.component';
 import { TodosService } from './services/todos.service';
