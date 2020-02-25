b56a4d5895b23856cdd3c7700954ce1b663a0af6
svensori <svensori@gmail.com>
2020-02-25 11:59:04 +0800

    Fix Errors on ng-form

--- src/app/ng-form/ng-form.module.ts
+++ src/app/ng-form/ng-form.module.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgFormComponent } from './ng-form.component';
@@ -9,11 +11,13 @@ import { NgFormComponent } from './ng-form.component';
 @NgModule({
     declarations: [NgFormComponent],
     imports: [
+        CommonModule,
         BrowserAnimationsModule,
         FormsModule,
         ReactiveFormsModule,
         MatFormFieldModule,
-        MatCardModule
+        MatCardModule,
+        MatInputModule
     ],
     exports: [NgFormComponent]
 })
