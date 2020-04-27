96a2b57677d2536de66797b35d44675711757103
svensori <svensori@gmail.com>
2020-04-27 15:04:02 +0800

    Unsubscribe to receiveMessage in socket-io

--- src/app/socket-io/socket-io.component.ts
+++ src/app/socket-io/socket-io.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { catchError, first } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 import { Message } from './interface/message';
 import { MessageSocketService } from './sockets/message-socket.service';
@@ -10,12 +11,14 @@ import { MessageSocketService } from './sockets/message-socket.service';
   templateUrl: './socket-io.component.html',
   styleUrls: ['./socket-io.component.scss']
 })
-export class SocketIoComponent implements OnInit {
+export class SocketIoComponent implements OnInit, OnDestroy {
 
   messages: Message[] = [];
   message = '';
   userNumber: number;
   inputControl: FormControl;
+
+  private subscription = new Subscription();
 
   constructor(
     private messageSocket: MessageSocketService
@@ -24,12 +27,11 @@ export class SocketIoComponent implements OnInit {
   ngOnInit() {
     this.inputControl = new FormControl('');
     this.startReceivingMessage();
-    this.messageSocket.getUserNumber()
-      .pipe(first())
-      .subscribe(
-        data => this.userNumber = data,
-        error => console.log('Please start the socket-io server by running: npm run start:socket-io')
-      );
+    this.getUserNumber();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   sendMessage() {
@@ -41,10 +43,20 @@ export class SocketIoComponent implements OnInit {
     this.inputControl.reset();
   }
 
-  startReceivingMessage() {
-    this.messageSocket.recieveMessage()
+  private startReceivingMessage() {
+    const receiveMessage$ = this.messageSocket.recieveMessage()
       .subscribe(message => {
         this.messages.push(message);
       });
+    this.subscription.add(receiveMessage$);
+  }
+
+  private getUserNumber() {
+    return this.messageSocket.getUserNumber()
+      .pipe(first())
+      .subscribe(
+        data => this.userNumber = data,
+        error => console.log('Please start the socket-io server by running: npm run start:socket-io')
+      );
   }
 }
